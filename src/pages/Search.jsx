9e9e02e08e393e1bc64c8165c60e1760ae410bc8
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2022-11-04 00:22:33 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  state = {
+    search: '',
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { search } = this.state;
     return (
       <div>
-        <div
+        <span
           data-testid="page-search"
         >
           Search
-        </div>
+        </span>
+        <form>
+          <label htmlFor="search">
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              onChange={ this.handleChange }
+              name="search"
+              id="search"
+              value={ search }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ search.length < +'2' }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
