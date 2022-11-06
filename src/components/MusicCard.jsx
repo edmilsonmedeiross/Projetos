91cd28ae09271c366d1e41104dabcb15271024eb
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2022-11-05 22:31:00 -0300

    requisito 8

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from '../pages/Loading';
 
 class MusicCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+    };
+  }
+
+  handleFavorites = async (infos) => {
+    this.setState({ isLoading: true });
+    await addSong(infos);
+    this.setState({ isLoading: false });
+  };
+
   render() {
     const { album } = this.props;
+    const { isLoading } = this.state;
     return (
       <div>
         <h3>Musics</h3>
@@ -21,9 +37,22 @@ class MusicCard extends Component {
                 <code>audio</code>
                 .
               </audio>
+              <label
+                htmlFor="favorites"
+              >
+                Favorita
+                <input
+                  onChange={ () => { this.handleFavorites(track); } }
+                  data-testid={ `checkbox-music-${track.trackId}` }
+                  type="checkbox"
+                  name="check"
+                  id={ track.trackId }
+                />
+              </label>
             </div>
           ))
         }
+        {isLoading && <Loading />}
       </div>
     );
   }
