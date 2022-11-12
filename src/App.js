1819e507696400500abbfa2dba38be8773ffc526
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2022-11-12 00:36:59 -0300

    fix css

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ class App extends React.Component {
     return (
       <div>
         <Switch>
-          <Route exact path="/" component={ Login } />
           <Route exact path="/search" component={ Search } />
           <Route exact path="/album/:id" component={ Album } />
           <Route exact path="/favorites" component={ Favorites } />
           <Route exact path="/profile" component={ Profile } />
           <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="/" component={ Login } />
           <Route component={ NotFound } />
         </Switch>
       </div>
